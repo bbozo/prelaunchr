9ff3d944bfc17c6fa9b3408d8b72807658a03328
Prelaunchr Website <prelaunchr@nitrokey-launch.dotplexserver.de>
2015-04-03 11:40:30 +0200

    flash error when IPs have been used up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -28,6 +28,7 @@ class UsersController < ApplicationController
                 )
             end
 
+            flash[:error] = "Your IP address #{request.env['HTTP_X_FORWARDED_FOR']} has been prevented from activating any more subscriptions"
             if cur_ip.count > 2
                 return redirect_to root_path
             else
